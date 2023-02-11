dfc2cb255b70f88e525f0ad4b9c49ef728223da6
LenaFarh <Lenafarh@mail.ru>
2023-02-11 18:04:20 +0300

    heder

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export const Footer =()=> {
+    return (
+    <div className = 'footer'>
+        <div className='contain'>
+    <div className='footer__links footer__group'>
+      <a href='/'>Контакты</a>
+      <a href='/'>Отзывы</a>
+      <a href='/'>Авторы проекта: </a>
+    </div>
+    </div>
+  </div>
+    )}
