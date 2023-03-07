20064062f242186883eae9a6a0c23f343b451bf1
LenaFarh <Lenafarh@mail.ru>
2023-03-07 15:09:36 +0300

    s dannymy

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -4,6 +4,8 @@ import { ReactComponent as Like } from '../../assets/icons/like.svg';
 import { useEffect, useState } from 'react';
 import { api } from '../../utils/Api';
 import dayjs from 'dayjs/';
+import { useNavigate } from 'react-router-dom';
+
 
 
 export const Post = ({ id }) => {
@@ -13,18 +15,14 @@ export const Post = ({ id }) => {
     api.getPostsById(id).then((data) => setPost(data))
   }, [id])
 
-  // добавить аватар автора <img src={post.author.avatar}....
-  // добавить имя автора {post.author.name}
-  // для того чтоб кнопка назад работала нужно использовать useNavigate посмотри как у меня в дог фуде 
-
-
+const navigate= useNavigate();
 
   return (
     <>
-      <div className='post_card'>
-        <button className='post_btn btn__type__primary'>Назад</button>
+    <button className='post_btn btn__type__primary' onClick={()=>navigate(-1)}>{'< '}Назад</button>
+      <div className='post_card'> 
         <div className='post_header'>
-          <div className='post__title'>{post.title}</div>
+          <div className='post__title'><h2>{post.title}</h2></div>
         </div>
         <div className='post__desc'>
           <div className='post_imgWrapper'>
@@ -33,14 +31,20 @@ export const Post = ({ id }) => {
               alt='изображение' />
           </div>
           <div className='post_info'>
-            <div>avatar</div>
-            <div>name</div>
-            <div>{dayjs(post.created_at).format("DD.MM.YYYY")}</div>
+            <div className='post_info__header'>
+            <img src={post?.author?.avatar} alt='avatar' className='post__author__ava'/>
+            <div className='post_author__name'><h3>{post?.author?.name}</h3></div>
+            </div>
+            <div>{dayjs(post.created_at).format("DD.MM.YYYY")}
+            <div>
+            <button className='post__favorite'>
+                <Like />
+              {post?.likes?.length !== 0 && <span className='post__likes'>{post?.likes?.length}</span>}
+              </button>
+              </div>
+            </div>
             <div className='post__data__tag'>
               {post.tags?.map((e) => <span className='post__data__tag__span'>{e}</span>)}
-              <button className='post__favorite'>
-                <Like />
-              </button>
             </div>
           </div>
         </div>
