063a280a3cf4d0a1e6c1f133d83bf990d71da31d
Alexey Martynenko <mr.m_alex@mail.com>
2023-04-21 14:21:49 +0300

    added part of redux

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,10 +3,10 @@ import { Footer } from '../footer/Footer';
 import React, { useEffect, useState } from 'react';
 import { Header } from '../header/Header';
 import { api } from '../../utils/Api';
-import { findLike, sortCards, useDebounce } from '../../utils/utils';
+import { sortCards, useDebounce } from '../../utils/utils';
 import { UserContext } from '../../context/UserContext'
 import { CardContext } from '../../context/CardContext'
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { CatalogPage } from '../../pages/catalogPage/CatalogPage';
 import { PostPage } from '../../pages/postPage/PostPage';
 import { Page404 } from '../../pages/page404/Page404';
@@ -16,24 +16,25 @@ import { Register } from '../auth/register/Register';
 import { ResetPass } from '../auth/resetPassword/ResetPassword';
 import { Profile } from '../profile/Profile';
 import { Favourites } from '../../pages/favorites/Favorites';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUser } from '../../storage/userSlice/userSlice';
+import { fetchCards } from '../../storage/cardsSlice/cardsSlice';
+
 
 
 function App() {
 
-  const [cards, setCards] = useState([]);
+  const [cardss, setCards] = useState([]);
   const [statSarch, setSarch] = useState('');
-  const [currentUser, setCurrentUser] = useState({});
-  const [favourites, setFavourites] = useState([]);
   const [isAuthentificated, setIsAuthentificated] = useState(false)
-  const [activeModal, setShowModal] = useState(false)
+  const [activeModal, setShowModal] = useState(false);
 
-  const filterCards = (dataCards) => {
-    const newCards = dataCards.filter((e) => e.author._id === '63ed527759b98b038f77b67f' || e.author._id === '63ee62853aa285034f78ab18')
-    return newCards;
-  }
+  const dispatch = useDispatch();
+  const cards = useSelector(s => s.cards.posts);
+
 
   const handleSearch = (search) => {
-    api.searchPosts(search).then((data) => setCards(filterCards(data)));
+    api.searchPosts(search).then((data) => setCards(data));
   };
 
   const debounceValueInApp = useDebounce(statSarch, 500);
@@ -42,38 +43,14 @@ function App() {
     handleSearch(debounceValueInApp);
   }, [debounceValueInApp]);
 
+useEffect(() =>{
+  if (!isAuthentificated) return;
+  dispatch(fetchUser()).then(() => dispatch(fetchCards()));
+}, [isAuthentificated, dispatch]);
 
   useEffect(() => {
-    Promise.all([api.getUserInfo(), api.getAllPosts()]).then(
-      ([userData, posts]) => {
-        setCurrentUser(userData);
-        setCards(filterCards(posts));
-        const fav = posts.filter((e) => findLike(e, userData));
-        setFavourites(fav);
-      }
-    );
-  }, [isAuthentificated]);
-
-  function handlePostLike(posts) {
-    const isLiked = posts.likes.some(id => id === currentUser._id);
-    api.changeLikePostStatus(posts._id, isLiked).then((newCard) => {
-      const newCards = cards.map((e) => e._id === newCard._id ? newCard : e);
-      setCards([...newCards]);
-      setFavourites((favour) => !isLiked ? [...favour, newCard] : favour.filter((f) => f._id !== newCard._id) );
-    });
-    return isLiked;
-  }
-
-  function deleteOwnPost(posts) {
-    if (posts.author._id === currentUser._id && window.confirm('Are you sure?')) {
-      api.deletePost(posts._id).then(() => {
-        const newPosts = cards.filter((el) => el._id !== posts._id);
-        setCards([...newPosts]);
-      });
-    } else if (posts.author._id !== currentUser._id) {
-      window.alert("You can't delete this post");
-    }
-  }
+    setCards(cards);
+  },[cards])
 
   function setSortCards(sort) {
     const cardSorted = sortCards(cards, sort);
@@ -82,23 +59,17 @@ function App() {
 
   const contextCardValue = {
     cards,
-    handlePostLike,
-    deleteOwnPost,
-    favourites,
   };
 
   const contextUserValue = {
-    currentUser,
     setSarch,
     setSortCards,
     statSarch,
     isAuthentificated,
     setIsAuthentificated,
-    setCurrentUser,
   };
 
   const navigate = useNavigate();
-  const location = useLocation();
 
   useEffect(() => {
     const token = localStorage.getItem('token')
